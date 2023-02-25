dfd2f188ab963276900926f2a54eb1c4101cb85b
Jack <jacklooi@github.com>
2023-02-25 17:48:11 +0800

    hook: add history hook store data on local storage

---
+++ src/hooks/useHistory.jsx
@@ -0,0 +1,23 @@
+import { useCallback, useState } from 'react';
+
+const useHistory = (key) => {
+  const initialState = [];
+  const historyString = localStorage.getItem(key);
+  const [value, setValue] = useState(historyString ? JSON.parse(historyString) : initialState);
+
+  const updatedHistory = useCallback(
+    (newValue) => {
+      if (newValue === initialState || typeof newValue === 'undefined') {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, JSON.stringify(newValue));
+      }
+      setValue(newValue ?? initialState);
+    },
+    [initialState, key],
+  );
+
+  return [value, updatedHistory];
+};
+
+export default useHistory;
